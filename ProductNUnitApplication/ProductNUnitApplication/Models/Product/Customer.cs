76134fcf479c15273c77616240fe1ecb5246bd55
Kamalpreet Singh <kamalpreetsingh025@gmail.com>
2018-09-11 18:16:31 +0530

    Basic Unit Testing Application using NUnit

---
+++ ProductNUnitApplication/ProductNUnitApplication/Models/Product/Customer.cs
@@ -0,0 +1,16 @@
+﻿/*
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductNUnitApplication.Models.Product
+{
+    public class Customer
+    {
+        public CustomerType CustomerType { get; set; }
+
+    }
+    public enum CustomerType { Regular, Premium }
+}
+*/
---
+++ ProductNUnitApplication/ProductNUnitApplication/Models/Product/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductNUnitApplication.Models.Product
+{
+    public class Product
+    {
+        public int ProductID { get; set; }
+        public string ProductName { get; set; }
+        public double ProductPrice { get; set; }
+        public CustomerType CustomerType { get; set; }
+    }
+
+    public enum CustomerType { Regular, Premium }
+
+}
